8605fc64a70797fd60936643220c710d3adf51cf
vincemilo <vincemilo@gmail.com>
2022-07-14 19:04:43 -0600

    finished first part of project

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,54 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:notice] = 'Successfully created.'
+      redirect_to @kitten
+    else
+      flash.now[:notice] = 'Invalid entry, please try again.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:notice] = 'Successfully updated.'
+      redirect_to @kitten
+    else
+      flash.now[:notice] = 'Invalid entry, please try again.'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:notice] = 'Successfully deleted.'
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
